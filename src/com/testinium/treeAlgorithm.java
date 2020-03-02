0ed2340a87f0691d8241aa80bce0e1b69dd9c445
Yilmaz Akkan <yilmaz.akkan@gmail.com>
2020-03-02 03:34:14 +0300

    Initial commit

---
+++ src/com/testinium/treeAlgorithm.java
@@ -0,0 +1,30 @@
+package com.testinium;
+
+import java.util.Scanner;
+
+public class treeAlgorithm {
+
+    public static void main(String[] args) {
+
+        try {
+            Scanner scanner = new Scanner(System.in);
+
+            Integer x = scanner.nextInt();
+
+            for (int i = 0; i <= x; i++) {
+
+                for (int j = x - 1; j >= i; j--) {
+                    System.out.print(" ");
+                }
+                for (int k = 1; k <= (2 * i + 1); k++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+
+        } catch (Exception e) {
+            System.out.println("You Must Enter a Integer Value !!!");
+        }
+
+    }
+}
